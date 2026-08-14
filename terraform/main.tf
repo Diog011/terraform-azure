024350bda6d05ee16915d6e57ca70ca6112e4147
diogo <RM561541@fiap.com.br>
2026-08-14 01:22:20 +0000

    Incrementando o container

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,9 +17,11 @@ resource "azurerm_resource_group" "rg" {
 }
 resource "azurerm_storage_container" "medallion-bronze" {
   name                  = "bronze"
-  storage_account_id    = azurerm_storage_account.storage.id
+  storage_account_id    = azurerm_storage_account.storage.name
   container_access_type = "private"
 }
+
+
 
 resource "azurerm_storage_account" "storage" {
  name = var.storage_account_name
